c226a2076ce5bf9ee3b556ede282f357c2dea78f
likahey99 <2661378h@student.gla.ac.uk>
2024-06-13 23:29:27 +0100

    Added mobile first navbar which also works on desktop
    It currently links into index but i will change it to link into base
    just a small change.

--- gucc_app/urls.py
+++ gucc_app/urls.py
@@ -2,6 +2,19 @@ from django.urls import path
 
 from . import views
 
+app_name = 'gucc_app'   
+
 urlpatterns = [
-    path('', views.index, name=''),  
+    path('', views.index, name='index'), 
+    path('about/', views.about, name='about'),
+    path('events/', views.events, name='events'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('main-shed/', views.main_shed, name='main_shed'),
+    path('macpherson-shed/', views.macpherson_shed, name='macpherson_shed'),
+    path('profile/', views.profile, name='profile'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout, name='logout'),
+    
+
 ]
--- gucc_app/views.py
+++ gucc_app/views.py
@@ -3,3 +3,31 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'gucc_app/index.html')
+
+def about(request):
+    return render(request, 'gucc_app/about.html')
+
+def events(request):    
+    return render(request, 'gucc_app/events.html')
+
+def gallery(request):    
+    return render(request, 'gucc_app/gallery.html')   
+
+def main_shed(request):     
+    return render(request, 'gucc_app/main-shed.html')
+
+def macpherson_shed(request):
+    return render(request, 'gucc_app/macpherson-shed.html')
+
+def profile(request):
+    return render(request, 'gucc_app/profile.html')
+
+def login(request):   
+    return render(request, 'gucc_app/login.html')   
+
+def register(request):
+    return render(request, 'gucc_app/register.html')
+
+def logout(request):    
+    return render(request, 'gucc_app/logout.html')
+
