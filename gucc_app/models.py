bc37abf7052c08b8d6434ac301e125d5f3f11806
Alex Deverill <alexdeverill35@gmail.com>
2024-06-17 22:29:58 +0100

    added migrations to gitignore

--- gucc_app/models.py
+++ gucc_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    position = models.CharField(max_length=20, default="member")
+    admin = models.BooleanField(default=False)
+
+class ItemType(models.Model):
+    name = models.CharField(max_length=120, null=False)
+    attributes = models.CharField(max_length=1000, null=False) #<name:>
+
+class Item(models.Model):
+    
